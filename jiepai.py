feaf23c32c05cd379d70b7780165be9c149de3a0
david nio <perfect_wjm@163.com>
2019-06-03 17:28:57 +0800

    add ajax spider files

---
+++ jiepai.py
@@ -0,0 +1,22 @@
+import requests
+from urllib.parse import urlencode
+
+
+def get_page(offset):
+    pass
+
+
+def get_image(json):
+    pass
+
+
+def save_image(item):
+    pass
+
+
+def main(offset):
+    pass
+
+
+if __name__ == "__main__":
+    pass
